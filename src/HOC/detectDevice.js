fa41064ccad8a17a9bca7adfafbc3f6841c436dc
Alice de Bonnechose <alice@monechelle.com>
2018-05-14 17:04:35 +0200

    first commit

--- src/HOC/detectDevice.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const detectDevice = (ModifyComponent) => {
-    return (props) => <ModifyComponent data={window.innerWidth > 640 ? 'Desktop' : 'Mobile'} {...props}/>
-};
-
-export default detectDevice;
